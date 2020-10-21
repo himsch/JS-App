78ccd4a861dea75745eb3aa8b322483dee4661fc
himsch <55rsearchco@gmail.com>
2020-10-21 09:09:01 +0900

    todo part one

---
+++ todo.js
@@ -0,0 +1,36 @@
+const toDoForm = document.querySelector(".js-toDoForm"),
+  toDoInput = toDoForm.querySelector("input"),
+  toDoList = document.querySelector(".js-toDoList");
+
+const TODOS_LS = "toDos";
+
+function paintToDo(text) {
+  const li = document.createElement("li"); // li 태그 생성
+  const delBtn = document.createElement("button");
+  delBtn.innerText = "❌";
+  const span = document.createElement("span");
+  span.innerText = text;
+  li.appendChild(span); // 무언가를 부모에게 append 하는거, li태그에 span태그 append
+  li.appendChild(delBtn);
+  toDoList.appendChild(li);
+}
+
+function handleSubmit(event) {
+  event.preventDefault(); // event 금지, submit 후 새로고침 금지
+  const currentValue = toDoInput.value;
+  paintToDo(currentValue);
+  toDoInput.value = "";
+}
+
+function loadToDos() {
+  const toDos = localStorage.getItem(TODOS_LS);
+  if (toDos !== null) {
+  }
+}
+
+function init() {
+  loadToDos();
+  toDoForm.addEventListener("submit", handleSubmit);
+}
+
+init();
