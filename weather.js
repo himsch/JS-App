7c1e73c3ef6c9d22ef4c27a3b5afa335655dba2f
himsch <55rsearchco@gmail.com>
2020-12-18 15:55:31 +0900

    weather API add

--- weather.js
+++ weather.js
@@ -1,4 +1,19 @@
+const weather = document.querySelector(".js-weather");
+
+const API_KEY = "2d3442dca5bbda2e85194a16dadc0b6b"
 const COORDS = 'coords';
+
+function getWeather(lat, lng){
+    fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
+        ).then(function(response){
+            return response.json()
+    }).then(function(json){
+        const temperature = json.main.temp;
+        const place = json.name;
+        weather.innerText = `${temperature} @ ${place}`
+    })
+}
 
 function saveCoords(coordsObj){
     localStorage.setItem(COORDS, JSON.stringify(coordsObj));
@@ -12,6 +27,7 @@ function handleGeoSucces(position){
         longitude
     };
     saveCoords(coordsObj);
+    getWeather(latitude, longitude);
 }
 
 function handleGeoError(){
@@ -27,7 +43,8 @@ function loadCoords(){
     if(loadedCoords === null){
         askForCoords();
     } else {
-        // getWeather
+        const parseCoords = JSON.parse(loadedCoords);
+        getWeather(parseCoords.latitude, parseCoords.longitude)
     }
 }
 
